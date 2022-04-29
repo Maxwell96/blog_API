6ac5fcb2ffad2eaa845b8facba76b4cea150e5b5
Maxwell Amoako Agyapong <agyapongmaxwell26@gmail.com>
2022-04-29 17:12:08 +0000

    Created a post model and views

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from email.quoprimime import body_check
+from ssl import create_default_context
+from tkinter import CASCADE
+from turtle import title
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
---
+++ posts/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
