3024a78083187a03a68ac64105362d1b377a5bd2
Maxwell Amoako Agyapong <agyapongmaxwell26@gmail.com>
2022-05-10 16:19:50 +0000

    Added a list and detail view for displaying all users

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .permissions import IsAuthorOrReadOnly
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
+from django.contrib.auth import get_user_model
 
 class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
@@ -11,3 +12,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
+class UserList(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+
+class UserDetail(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
